fb95a41432ae9e00cdb2cfced89dda477a8c618e
Micaela Pastor <micaelapastor99@gmail.com>
2022-09-28 14:48:16 -0300

    Pastor_Micaela_LoopDLoop.js

---
+++ Pastor_Micaela_LoopDLoop.js
@@ -0,0 +1,24 @@
+
+For(var kilometro = 0; kilometro <= 10 ; kilometro ++) { 
+
+    if (Kilometro=0){
+        KilometroCaramelo=0;
+    }
+    else{
+        KilometroCaramelo +=1
+    }
+
+    if (kilometroCaramelo=3){
+        caramelo+=1
+        kilometroCaramelo-=3
+    }
+
+    console.log (caramelo);
+    console.log (kilometro);
+}
+//Necesitamos un bucle porque existe una accion repetitiva, dar un caramelo cada 3km.
+//El punto de partida es 0
+//El bucle debe detenerse en 10km.
+//Sabrá parar cuando la var. km sea=10
+//El incremento es por km, ya que la trotadora los cuenta de 1 en 1.
+//Las variables necesarias son la de kilometro, kilometrocaramelo y caramelo.
