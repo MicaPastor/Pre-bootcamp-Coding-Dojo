c7db7d1eb1a700795b012f291ec45c754d660fd8
Micaela Pastor <micaelapastor99@gmail.com>
2022-09-29 15:18:47 -0300

    Pastor_Micaela_JSPrediccion.js

---
+++ Pastor_Micaela_JSPrediccion.js
@@ -0,0 +1,16 @@
+Function myBirthYearFunc () {
+    console.log ("Naci en" + 1980)
+} //Console.log indicará "Naci en1980" 
+
+Function myBirthYearFunc(birthYearInput){
+    console.log("Naci en" + birthYearInput);
+} /7 El console.log indicará "Nací en1980"
+
+Function add(num1, num2){
+    console.log("Sumando números");
+    console.log("num1 es: " + num1);
+    console.log("num2 es: " + num2)
+    var sum = num1 + num2;
+    console.log(sum);
+}
+// num1 es 10, num2 es 20, por lo tanto sum es 30.
